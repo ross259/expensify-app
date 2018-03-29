6d7c74d3985383bdb50e554d63549205f9e1cbd5
Ross Burke <ross.burke@gmail.com>
2018-03-28 19:08:52 -0600

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -18,16 +18,16 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-  console.log(visibleExpenses);
-  // console.log(state)
-})
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+//   console.log(visibleExpenses);
+//   // console.log(state)
+// })
 
-store.dispatch(addExpense({description:'Water Bill', amount:4500, createdAt:1000}));
-store.dispatch(addExpense({description: 'Gas Bill', amount:3400, createdAt:1005}));
-store.dispatch(addExpense({description: 'Rent', amount:109500, createdAt:995}));
+// store.dispatch(addExpense({description:'Water Bill', amount:4500, createdAt:1000}));
+// store.dispatch(addExpense({description: 'Gas Bill', amount:3400, createdAt:1005}));
+// store.dispatch(addExpense({description: 'Rent', amount:109500, createdAt:995}));
 
 // store.dispatch(setTextFilter('bill'));
 // store.dispatch(setTextFilter('water'));
