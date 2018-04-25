41177fa89eeefa7b43f3daa965a2220342f478c6
Ross Burke <ross.burke@gmail.com>
2018-04-24 19:39:26 -0600

    Changed API_URL default

--- src/db/db_mongo.js
+++ src/db/db_mongo.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.API_URL || '/api/';
 
 const get = (node) => {
   // console.log (`HERE::: ${API_URL}${node}`);
