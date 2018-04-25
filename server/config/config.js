f5401f031150bf91af595ee7742bfbf4c822a7ab
Ross Burke <ross.burke@gmail.com>
2018-04-24 18:18:11 -0600

    Set DB to mongo, updated DB config with Mongolab connection strings

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,8 @@ if (env === 'development'){
   process.env.DB_CONNECT = 'mongodb://localhost:27017/expensify';
 }else if (env === 'test'){
   // process.env.DB_CONNECT = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds135963.mlab.com:35963/${process.env.DB_NAME}_test`;
+  process.env.DB_CONNECT = 'mongodb://test_user:trains1234!@ds161304.mlab.com:61304/expensify_test';
 }else if (env === 'production'){
-  // process.env.DB_CONNECT = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds121171.mlab.com:21171/${process.env.DB_NAME}`;
+  // process.env.DB_CONNECT =`mongodb://ross259:w0Rd2urM0M!@ds157599.mlab.com:57599/expensify`;
+  // SERVER ENVIRONMENT VARIABLE
 }
